2a02c7cde3713da70af83b87b43799da61acde40
Otavio <otamm@hotmail.com>
2015-01-04 19:39:44 -0200

    Added custom variables + random facts.

--- Age of Doge/Age of Doge/ViewController.swift
+++ Age of Doge/Age of Doge/ViewController.swift
@@ -18,6 +18,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var comradeDogeWords: UILabel!
     
+    //custom variables
+    
+    var wordsOfWisdom: [String] = ["Did yuo kno?This is a human age to dog age converter (aka multiply by 7). If you really need this, woof such dumb!", "Did yuo kno? If Chairman Stalin were alive in 1957, he would also have killed Comrade Laika, woof!","Did yuo kno? Comrade Laika became a postcard in Romania celebrating the first cosmonaut, woof!","Did yuo kno? Comrade Laika was born in 1954, weighed 5kg and was a streetdog in Moscowgrad, woof!","Did yuo kno? Comrade Laika was already intended to die when Sputnik 2 was launched, woof! She died of the machines overheating, much shit, woof!","Did yuo kno? Comrade Laika had a monument of her built in 2008 near the space station in Moscowgrad that launched her, woof!"]
+    
+    var wordsEnumeration = 0
     
     //generated functions
     override func viewDidLoad() {
